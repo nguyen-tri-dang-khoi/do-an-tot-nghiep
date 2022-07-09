eb445932a71ada75dcff4e91cc2ab2e5d0ec7f51
nguyen-tri-dang-khoi <nguyentridangkhoi@gmail.com>
2022-07-09 13:37:14 +0700

    ok hi

--- admin/ajax_company_info.php
+++ admin/ajax_company_info.php
@@ -10,24 +10,29 @@
         <div class="col-xl-4 col-md-6 col-sm-12 form-group">
             <label for="full_name">Tên công ty</label>
             <input type="text" class="form-control" name="company_name" placeholder="Nhập tên công ty" value="<?=$result['company_name']?>">
+            <p id="company_name_err" class="text-danger"></p>
         </div>
         <div class="col-xl-4 col-md-6 col-sm-12 form-group">
             <label for="email">Email công ty</label>
             <input type="email" class="form-control" name="company_email" placeholder="Nhập email công ty" value="<?=$result['company_email']?>">
+            <p id="company_email_err" class="text-danger"></p>
         </div>
         <div class="col-xl-4 col-md-12 form-group">
             <label for="phone">Số điện thoại công ty</label>
             <input type="number" min="1" class="form-control" name="company_phone" placeholder="Nhập số điện thoại công ty" value="<?=$result['company_phone']?>">
+            <p id="company_phone_err" class="text-danger"></p>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6 col-sm-12 form-group">
             <label for="email">Mã số thuể</label>
             <input type="number" min="1" class="form-control" name="company_tax_code" placeholder="Nhập mã số thuế công ty" value="<?=$result['company_tax_code']?>">
+            <p id="company_tax_code_err" class="text-danger"></p>
         </div>
         <div class="col-md-6 col-sm-12 form-group">
             <label for="address">Địa chỉ</label>
             <input type="text" class="form-control" name="company_address" placeholder="Nhập địa chỉ thường trú" value="<?=$result['company_address']?>">
+            <p id="company_address_err" class="text-danger"></p>
         </div>
     </div>
 </div>
--- admin/ajax_information.php
+++ admin/ajax_information.php
@@ -11,14 +11,17 @@
         <div class="col-xl-4 col-md-6 col-sm-12 form-group">
             <label for="full_name">Tên đầy đủ</label>
             <input type="text" class="form-control" name="full_name" placeholder="Nhập họ tên đầy đủ" value="<?=$result['full_name']?>">
+            <p id="full_name_err" class="text-danger"></p>
         </div>
         <div class="col-xl-4 col-md-6 col-sm-12 form-group">
             <label for="email">Email</label>
             <input type="email" class="form-control" name="email" placeholder="Nhập email" value="<?=$result['email']?>">
+            <p id="email_err" class="text-danger"></p>
         </div>
         <div class="col-xl-4 col-md-12 form-group">
             <label for="phone">Số điện thoại</label>
             <input type="number" min="1" class="form-control" name="phone" placeholder="Nhập số điện thoại" value="<?=$result['phone']?>">
+            <p id="phone_err" class="text-danger"></p>
         </div>
     </div>
     <div class="row">
@@ -28,6 +31,7 @@
                 <input name="img_admin_file" type="file" class="custom-file-input" id="fileInput" onchange="readURL(this)">
                 <label class="custom-file-label" for="fileInput">Chọn ảnh</label>
             </div>
+            <p id="img_name_err" class="text-danger"></p>
             <div class="img-fluid" id="where-replace">
                 <img src="<?=$result['img_name'] ? "../admin/".$result['img_name'] : "upload/noimage.jpg";?>" class="img-fluid" id="display-image"/>
             </div>
@@ -37,10 +41,12 @@
         <div class="col-md-6 col-sm-12 form-group" >
             <label for="birthday">Ngày sinh</label>
             <input type="text" class="form-control kh-datepicker" name="birthday" placeholder="Nhập ngày tháng năm sinh" value="<?=Date('d-m-Y',strtotime($result['birthday']));?>">
+            <p id="birthday_err" class="text-danger"></p>
         </div>
         <div class="col-md-6 col-sm-12 form-group">
             <label for="address">Địa chỉ</label>
             <input type="text" class="form-control" name="address" placeholder="Nhập địa chỉ thường trú" value="<?=$result['address']?>">
+            <p id="address_err" class="text-danger"></p>
         </div>
     </div>
 </div>
@@ -54,14 +60,17 @@
             <div class="col-lg-4 col-md-12 form-group">
                 <label for="">Mật khẩu cũ:</label>
                 <input type="password" class="form-control" name="old_pass" placeholder="Nhập mật khẩu cũ..." value="">
+                <p id="old_pass_err" class="text-danger"></p>
             </div>
             <div class="col-lg-4 col-md-12 form-group">
                 <label for="">Mật khẩu mới:</label>
                 <input type="password" class="form-control" name="new_pass" placeholder="Nhập mật khẩu mới..." value="">
+                <p id="new_pass_err" class="text-danger"></p>
             </div>
             <div class="col-lg-4 col-md-12 form-group">
                 <label for="">Xác nhận mật khẩu mới:</label>
                 <input type="password" class="form-control" name="confirm_new_pass" placeholder="Nhập xác nhận mật khẩu mới..." value="">
+                <p id="confirm_new_pass_err" class="text-danger"></p>
             </div>
         </div>
     </div>
